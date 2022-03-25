63e023dae51589c84de12225cebcb1b87e7ead81
mshnschnko <misha321654987i@gmail.com>
2022-03-25 18:50:50 +0300

    mistakes fixed

--- QuadraticEq/src/QuadraticEqHeader.hpp
+++ QuadraticEq/src/QuadraticEqHeader.hpp
@@ -1,3 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+
+typedef struct quadEq {
+	double a, b, c;
+};
--- QuadraticEq/src/QuadraticEqSolver.cpp
+++ QuadraticEq/src/QuadraticEqSolver.cpp
@@ -1,39 +1,48 @@
 #include "QuadraticEqHeader.hpp"
 
-int solution(const double a, const double b, const double c, double& x1, double& x2)
+int solver(quadEq& eq, double& x1, double& x2)
 {
-	double discriminant = b * b - 4 * a * c;
-	if (discriminant < 0)
+	double discriminant = eq.b * eq.b - 4 * eq.a * eq.c;
+	if (discriminant < 0.0)
 		return 0;
-	else if (discriminant == 0)
+	else if (discriminant == 0.0)
 	{
-		x1 = x2 = -b / (2 * a);
+		x1 = x2 = -eq.b / (2 * eq.a);
 		return 1;
 	}
 	else
 	{
-		x1 = (-b + sqrt(discriminant)) / (2 * a);
-		x2 = (-b - sqrt(discriminant)) / (2 * a);
+		x1 = (-eq.b + sqrt(discriminant)) / (2 * eq.a);
+		x2 = (-eq.b - sqrt(discriminant)) / (2 * eq.a);
 		return 2;
 	}
 }
 
-int solver() {
-	double a, b, c;
-	std::cout << "please enter quotients a, b, c separating them by space: ";
-	std::cin >> a >> b >> c;
-	double x1, x2;
-	int rootsCount = solution(a, b, c, x1, x2);
-	if (rootsCount == 0)
+void printAnswer(int cnt, double x1, double x2) {
+	if (cnt == 0)
 		std::cout << "No roots";
-	else if (rootsCount == 1)
+	else if (cnt == 1)
 		std::cout << "x = " << x1;
 	else
 		std::cout << "x1 = " << x1 << "\nx2 = " << x2;
-	return 0;
+}
+
+quadEq readEq() {
+	quadEq eq;
+	std::cout << "please enter quotients a, b, c separating them by space: ";
+	std::cin >> eq.a >> eq.b >> eq.c;
+	return eq;
+}
+
+void getAnswer(quadEq& eq) {
+	double x1, x2;
+	int cnt = solver(eq, x1, x2);
+	printAnswer(cnt, x1, x2);
 }
 
 int main()
 {
-	return solver();
+	quadEq eq = readEq();
+	getAnswer(eq);
+	return 0;
 }
